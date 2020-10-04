9a77804d188ca4c7a4cc0e5fcc399eb6a6af7a96
Jarlez <62815538+Jarlez@users.noreply.github.com>
2020-10-04 19:16:51 -0300

    estacionamento

---
+++ src/pacclass/Sistema.java
@@ -0,0 +1,140 @@
+package pacclass;
+
+import java.util.Scanner;
+
+public class Sistema {
+
+    public static void main(String[] args) {
+   
+
+//int numero1, numero2, soma;
+      //numero1 = 5;
+      //numero2 = 5;
+             //soma=numero1 + numero2;
+              //int resto = soma % 2;
+              //System.out.println("A soma dos números é: "+ soma);
+        //System.out.println("O resto da divisão da soma dos números é: "+resto);
+    
+    
+    
+       //Scanner leia = new Scanner(System.in);
+    //System.out.println("Digite aqui seu nome");
+    
+    //String nome = leia.next();
+       
+    //System.out.println("Seu nome é " +nome);
+        
+        
+        //System.out.println("Digite aqui a sua altura");
+        //Double altura = leia.nextDouble();
+        //System.out.println("A sua altura é :"+altura);
+    
+    
+         
+        //Scanner leia = new Scanner(System.in);
+        //System.out.println("Informe um número decimal");
+        //double x = leia.nextDouble();
+        //int = (int) x;
+        //System.out.println("X:"+x);
+        //System.out.println("Y:"+n);
+        
+        
+        
+        //Scanner leia = new Scanner(System.in);
+        //System.out.println("Informe um valor inteiro"); 
+        //int n1 = leia.nextInt();
+        //System.out.println("Informe outro valor inteiro");
+        //int n2 = leia.nextInt(); 
+        
+        //if (n2>n1) {
+            //System.out.println(n1+","+n2);
+        //} else {
+          //System.out.println(n1 +","+n2);
+        //}
+        
+        
+        
+        //Scanner leia= new Scanner(System.in);
+        //System.out.println("Digite aqui um número");
+        //int n = leia.nextInt();
+        
+        //if (n%2 == 0) {
+            //System.out.println("O número é par");
+        //} else {
+            //System.out.println("O número é ímpar");
+        //}
+        
+        
+        
+       // --------------------------- Atividade Pães ----------------------
+        
+        //Scanner leia = new Scanner(System.in);
+                //System.out.println("Informe a quantidade de pães: ");
+                //int qtdePaes = leia.nextInt();
+                //System.out.println("Informe a quantidade de broas: ");
+                //int qtdeBroas = leia.nextInt();
+                
+                //double totalPaes = qtdePaes * 0.12;
+                //double totalBroas = qtdeBroas * 1.5;
+                //double total = totalPaes + totalBroas;
+                //double deposito = 0;
+                //if (total > 500) {
+                  //deposito = total * 0.15;
+        //} else {
+          //deposito = total * 0.15;
+                //}
+                
+                //System.out.println("Depósito R$"+deposito);
+                      
+                      //System.out.println("Total arrecadado : "+total); 
+    
+                      
+                      
+        //--------------------------------------------------------------------              
+                      
+                      
+       
+        
+//--------------------------------------------- V ou F -----------------------
+
+//Scanner leia = new Scanner(System.in) ; 
+                      //System.out.println("Informe o sexo : M ou F "); 
+                      //String sexo = leia.next();
+                      //if (sexo.equalsIgnoreCase("F")) {
+                          //System.out.println("Seu sexo é feminino");} 
+                      //else { System.out.println("Seu sexo é masculino");
+        //}
+   
+   
+    //---------------------------------------------------------------------
+        
+        
+        
+        
+        // ---------------------------------- Repetição ----------------
+        
+        
+        //int c = 1;
+        //System.out.println(c);
+        //while (c<=10) {
+            //System.out.println(c);            
+            //c++;
+        //}
+        
+     //---------------------------------------------------------------------  
+        
+        
+        
+     
+     //Scanner leia = new Scanner(System.in);
+        //System.out.println("Informe o nome de uma pessoa ou EXIT para sair do sistema");
+        //String nome = leia.next();
+        //while (!nome.equalsIgnoreCase("EXIT")){ System.out.println("A pessoa se chama "+nome);
+            //System.out.println("Informe outro nome de pessoa ou EXIT para sair");
+            //nome = leia.next();
+    
+            
+        //}       
+    }
+    
+   }
---
+++ src/sis/Estacionamento.java
@@ -0,0 +1,9 @@
+
+package sis;
+
+
+public class Estacionamento {
+    
+}
+
+
